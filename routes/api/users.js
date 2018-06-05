7f3e381d97ac4ea3b770980c8afc08426bce3f22
Travis Kane <kevinthomaskane@gmail.com>
2018-06-04 17:59:00 -0700

    server setup and initial routes

---
+++ routes/api/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// @route     GET api/users/test
+// @desc      (description) users post route
+// @access    Public
+
+router.get('/test', (req, res) => res.json({message: 'users works'}))
+
+module.exports = router;
