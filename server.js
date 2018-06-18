dd5adcfdd89d636b68dd6e28e89b88f6e4f3f128
Travis Kane <kevinthomaskane@gmail.com>
2018-06-17 17:53:50 -0700

    update server file for heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const emoji = require("node-emoji");
+const path = require("path")
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -32,6 +33,15 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+//server static assets if in production
+if(process.env.NODE_ENV === "production"){
+  //set static folder
+  app.use(express.static("client/build"))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
 
 const port = process.env.PORT || 5000;
 
