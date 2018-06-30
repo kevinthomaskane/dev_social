470040abd852cc476a5d2939870cccf5ea10fee0
Travis Kane <kevinthomaskane@gmail.com>
2018-06-29 23:38:35 -0700

    gen copy updates

---
+++ client/src/components/dashboard/advancedReact.md
@@ -0,0 +1,2 @@
+-webpack: links together JS files for production
+-jest: automated test runner, takes all the different test files and prints the output inside the terminal
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -61,7 +61,7 @@ class Navbar extends Component {
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
           <div className="container">
             <Link className="navbar-brand" to="/">
-              DevConnector
+              Dev Social
             </Link>
             <button
               className="navbar-toggler"
